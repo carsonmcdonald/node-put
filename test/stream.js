77d4b9dcb308ce49b52da2800c301bb792dad048
James Halliday <substack@gmail.com>
2010-10-15 03:17:19 +0000

    stream test

---
+++ test/stream.js
@@ -0,0 +1,20 @@
+var Put = require('put');
+
+exports.stream = function (assert) {
+    var written = 0;
+    var stream = {
+        write : function (buf) {
+            assert.equal(buf.toString(), 'abcdefg');
+            written ++;
+        }
+    };
+    
+    Put()
+        .word16be(24930)
+        .word32le(1717920867)
+        .word8(103)
+        .write(stream)
+    ;
+    
+    assert.equal(written, 1);
+};
