3ed91f0205e3c45485fa5f900382be5c5fcbcec0
James Halliday <mail@substack.net>
2011-04-20 17:21:38 -0700

    passes all the tests again finally

--- test/stream.js
+++ test/stream.js
@@ -12,14 +12,14 @@ exports.stream = function () {
     
     Put()
         .word16be(
-            'a'.charCodeAt(0)
-            + 256 * 'b'.charCodeAt(0)
+            256 * 'a'.charCodeAt(0)
+            + 'b'.charCodeAt(0)
         )
         .word32le(
-            Math.pow(256, 3) * 'c'.charCodeAt(0)
-            + Math.pow(256, 2) * 'd'.charCodeAt(0)
-            + Math.pow(256, 1) * 'e'.charCodeAt(0)
-            + 'f'.charCodeAt(0)
+            Math.pow(256, 0) * 'c'.charCodeAt(0)
+            + Math.pow(256, 1) * 'd'.charCodeAt(0)
+            + Math.pow(256, 2) * 'e'.charCodeAt(0)
+            + Math.pow(256, 3) * 'f'.charCodeAt(0)
         )
         .word8('g'.charCodeAt(0))
         .write(stream)
